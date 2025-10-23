11347be9c417f34ee0e8f4fbc4f30db2e2c69d3f
tryapitsynandrey-web <tryapitsynandrey@gmail.com>
2025-10-23 22:34:28 +0100

    Create a = {1, 2, 3}.py

---
+++ a = {1, 2, 3}.py
@@ -0,0 +1,11 @@
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.difference(b))  # {1, 2}
+print(a - b)  # {1, 2}
+
+num = 15  # приклад значення для num
+
+if num > 10:
+    print("num більше за 10")
+else:
+    print("num не більше за 10")
