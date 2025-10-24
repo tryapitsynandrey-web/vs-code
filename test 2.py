06d33bac342d2a394a533fdf847437d7a82b2f53
tryapitsynandrey-web <tryapitsynandrey@gmail.com>
2025-10-24 15:28:31 +0100

    Add FizzBuzz implementation and initial result check

---
+++ test 2.py
@@ -0,0 +1,12 @@
+# Задаємо конкретне число
+num = int(input())
+
+# Перевіряємо кратність
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print(num)
